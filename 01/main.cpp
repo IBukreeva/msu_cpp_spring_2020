ae8c64f9b60d272f0488a0f08b9b111e797ffa09
IBukreeva <62015022+IBukreeva@users.noreply.github.com>
2020-03-12 20:10:58 +0300

    Update main.cpp

--- 01/main.cpp
+++ 01/main.cpp
@@ -3,63 +3,43 @@
 #include <iostream>
 #include "alloc.h"
 
-using namespace std;
-extern char * mem;
-extern size_t maxSize;
-extern size_t pnt;
-
 int main(void){
 
-    //test1 - просто тест
     //до вызова makeAllocator память выделить нельзя, а после выделится
     {
-        char * a0 =alloc(100); //должно быть ничего
-        makeAllocator(1000);
+        char *a0, *a1, *a2, *a3, *a4, *a5;
 
-        char * a1 = alloc(100);
-        char * a4 = alloc(100);
+        //1
+        a0 =alloc(100); //должно быть ничего
         
+        makeAllocator(1000);
+
+        a1 = alloc(100); //выделяется
+        a4 = alloc(100); //выделяется
         if(a1!=nullptr && a0==nullptr && a4!=nullptr) std::cout<< "works1\n";
-    }
-    delete[] mem; mem=nullptr;
 
-    //test2 - постепенно просим больше, чем есть
-    //то есть 1й раз память выделится, а 2й раз должно быть nullptr
-    {
-        makeAllocator(1000);
-
-        char * a1 = alloc(500);
-        char * a2 = alloc(600);
-        if(a1!=nullptr && a2==nullptr) std::cout<< "works2\n";
-    }
-    delete [] mem; mem=nullptr;
-
-    //test3 - сразу просим больше, чем есть
-    //то есть ничего не выделится
-    {
-        makeAllocator(1000);
-
-        char * a1 = alloc(1500);
-        if(a1==nullptr) std::cout<< "works3\n";    
-    }
-    delete[] mem; mem=nullptr;
-
-
-
-    //test4 - просим до reset() меньше maxSize, после тоже меньше
-    // а в сумме больше maxSize
-    //то есть оба указателя существуют
-    {
-        makeAllocator(maxSize);
-
-        char * a1 = alloc(500);
         reset();
-        char * a5 = alloc(700);
+        
+        //2
+        a1 = alloc(500); //выделяется
+        a2 = alloc(600); //не выделяется
+        if(a1!=nullptr && a2==nullptr) std::cout<< "works2\n";
+
+        reset();
+
+        //3
+        a1 = alloc(1500); //не выделяется
+        if(a1==nullptr) std::cout<< "works3\n";   
+
+        reset();
+        
+        //4
+        a1 = alloc(500); //выделяется
+        reset(); 
+        a5 = alloc(700); //выделяется
         if(a1!=nullptr && a5!=nullptr) std::cout<< "works4\n";
     }
-    delete[] mem; mem=nullptr;
-
-
+    
 
     return 0;
 }
