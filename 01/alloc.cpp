3fb0c1f97d73f831291af5ba23bd7bd00edf09de
IBukreeva <62015022+IBukreeva@users.noreply.github.com>
2020-03-12 20:10:02 +0300

    Update alloc.cpp

--- 01/alloc.cpp
+++ 01/alloc.cpp
@@ -1,25 +1,23 @@
 
 #include <iostream>
 #include "alloc.h"
-size_t maxSize=1000;
+size_t maxSize;
 size_t pnt=0;
 char * mem=nullptr;
 
-void makeAllocator(size_t maxSize){
+void makeAllocator(size_t mSize){
+    maxSize=mSize;
     mem = (char*) malloc(maxSize*sizeof(char));
     pnt = 0;
-    //printf("makeAllocator:: %p\n", mem);
 }
 
 void reset(){
     pnt=0;
-
 }
 
 char * alloc(size_t size){
     if(mem==nullptr){ return nullptr;}
     if(pnt+size>maxSize){
-        //printf( "can't allocate memory");
         return nullptr;
     }
     char * ans = mem+pnt;
