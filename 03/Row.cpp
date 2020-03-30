2020fe3042c0dfea1e0c1906b0f663dfe504db31
IBukreeva <62015022+IBukreeva@users.noreply.github.com>
2020-03-30 12:26:43 +0500

    Update Row.cpp

--- 03/Row.cpp
+++ 03/Row.cpp
@@ -1,58 +1,56 @@
-#include "Row.h"
 #include <iostream>
+#include "Row.h"
 
-Row::Row(int inSize){
+Row::Row(size_t inSize){
+    this->init(inSize);
+}
+
+void Row::init(size_t inSize){
     data = new int[inSize];
     size=inSize;
 }
-    
+
 Row::~Row(){
-    free(data);
+    delete[] data;
     size=0;
 }
 
-int Row::getSize() const {
+size_t Row::getSize() const {
     return size;
-}
-
-bool Row::operator!=(Row& r) const{
-    if(size!=r.getSize()) return true;
-
-    for(int i=0;i<size;i++){
-        if(data[i]!=r.data[i]) return true;
-    }
-    return false;
 }
 
 bool Row::operator==(Row& r) const{
     if(size!=r.getSize()) return false;
 
-    for(int i=0;i<size;i++){
+    for(size_t i=0;i<size;i++){
         if(data[i]!=r.data[i]) return false;
     }
-
     return true;
-}  
-
-void Row::operator *= (int x){
-    for(int i=0;i<size;i++){
-        data[i]*=x;
-    }
 }
 
-int& Row::operator [](int n) const {
-    if(n<0 || n>=size){
+bool Row::operator!=(Row& r) const{
+    return !(*this==r);
+}
+
+Row& Row::operator *= (int x){
+    for(size_t i=0;i<size;i++){
+        data[i]*=x;
+    }
+    return *this;
+}
+
+int& Row::operator [](size_t n) const {
+    if(n>=size){
         throw std::out_of_range("");
     }
     return data[n];
 }
 
 Row& Row::operator =(Row& r){
-    this->~Row(); 
-
-    size=r.getSize();
-    data = new int[size];
-    for(int i=0;i<size;i++){
+    
+    delete[] data;
+    this->init(r.getSize());
+    for(size_t i=0;i<size;i++){
         data[i]=r.data[i];
     }
     return *this;
