cc72ec7eae4018e4399b3ba1515dff141d162b9f
IBukreeva <62015022+IBukreeva@users.noreply.github.com>
2020-04-22 01:17:35 +0500

    Update format.h

--- 06/format.h
+++ 06/format.h
@@ -1,50 +1,58 @@
+#include <string>
+#include <sstream>
+#include <vector>
 
-enum class Error{
-    RuntimeError,
-    NoError
-};
-
-Error process(std::stringstream& str, size_t cur, size_t need) noexcept{
-    return Error::RuntimeError;
-}
-
-template <class T, class... Args>
-Error process(std::stringstream& str, size_t cur, size_t need, T&& val, Args&&... args) noexcept{
-    if(cur<need){
-        return process(str, cur+1, need, std::forward<Args>(args)...);
-    }
-    else{ 
-        str << val; 
-        return Error::NoError;
-    }
+template<class T>
+std::string argsToStr(T&& val){
+    std::stringstream ans;
+    ans << val;
+    return ans.str();
 }
 
 template <class... Args>
-std::string format(const char * text, Args&&... args){ 
-    std::stringstream ans;
-    size_t len_text = strlen(text);
-    size_t i=0;
-    while(i<len_text){
-        if(text[i]=='{'){
-            if(i+2>=len_text) throw std::runtime_error("");
-            else{
-                if(text[i+2]=='}' && std::isdigit(text[i+1])){
-                    Error er = process(ans, 0, text[i+1]-'0', std::forward<Args>(args)...);
-                    if(er==Error::RuntimeError) throw std::runtime_error("");
-                    i+=3;
-                }
-                else throw std::runtime_error("");
+std::string format(const std::string& text, Args&&... args){ 
+    std::stringstream answer;
+    size_t len_text = text.size();
+    std::vector<std::string> arg_data = {argsToStr(std::forward<Args>(args))... };
+    size_t ind_arg = 0;
+    size_t size_arg = arg_data.size();
+
+    bool flaq = false; //этот флаг имеет состояния: false - ничего не происходит, true - открыта фигурная скобка
+    
+    for(size_t i=0; i<len_text; i++){
+        if(flaq){ //то мы ждем или цифру, или закрывающую скобку
+            if(std::isdigit(text[i])){
+                ind_arg *= 10; 
+                ind_arg += text[i]-'0';
             }
+            else if(text[i]=='}'){
+                if(ind_arg>=size_arg)
+                    throw std::runtime_error("not so many args");
+                else{
+                    answer << arg_data[ind_arg];
+                    flaq = false;
+                }
+            }
+            else
+                throw std::runtime_error("opening bracket without closing one");
+            
         }
-        else if(text[i]=='}'){ //то есть закрывающая без открывающей
-            throw std::runtime_error("");
-        }
-        else{
-            ans << text[i];
-            i+=1;
+        else { //то мы ждем открывающую скобку или не закрывающую скобку или любой другой символ
+            if(text[i]=='{'){
+                flaq = true;
+                ind_arg = 0;
+            }
+            else if(text[i]=='}')
+                throw std::runtime_error("closing bracket without the opening");
+            else 
+                answer<<text[i];
         }
     }
-    return ans.str();
+
+    if(flaq){ //то есть строка закончилась, а скобочку не закрыли
+        throw std::runtime_error("no closing bracket in the end");
+    }
+    return answer.str();
 
 }
 
